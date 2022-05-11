120d5c17797e0e09026c4e86190bfcc6556982c7
Christopher A. Stone <stone@cs.hmc.edu>
2022-05-11 11:16:25 -0700

    Make sentize2.py nicer

--- sentize2.py
+++ sentize2.py
@@ -1,29 +1,29 @@
 #!/usr/bin/env python
 
-"""
-Extracts proofs from .tex files.
-"""
+"""Split lines into sentences."""
 
-import argparse
-import re
 import sys
+from typing import List
 
 from nltk.tokenize import PunktSentenceTokenizer
 from nltk.tokenize.destructive import NLTKWordTokenizer
 
+import nicer
+
 # suspicious = re.compile("[&_^\x00-\x1f\x80-\xff]")
 
 if __name__ == "__main__":
+    nicer.make_nice()
+
     for filename in sys.argv[1:]:
         with open(filename, "r") as fd:
             sent_tokenizer = PunktSentenceTokenizer()
             word_tokenizer = NLTKWordTokenizer()
-            print("ok")
             for line in fd.readlines():
                 # if re.search(suspicious, line):
                 #     continue
                 text = line.strip()
-                sents = list(sent_tokenizer.tokenize(text))
+                sents: List[str] = list(sent_tokenizer.tokenize(text))
                 # Remove parentheses around parenthesized sentences
                 for sent in sents:
                     if sent[0] == "(" and sent[-1] == ")":
@@ -32,7 +32,3 @@ if __name__ == "__main__":
                             sent = interior
                     words = word_tokenizer.tokenize(sent)
                     print(" ".join(words))
-            # # Sort
-            # sent.sort()
-            # for s in sent:
-            #     print(s)
