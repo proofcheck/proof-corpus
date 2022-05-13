4b06b12f424fcf68b48973574fd2a9565196c942
Christopher A. Stone <stone@cs.hmc.edu>
2022-05-13 12:54:33 -0700

    Improvements to linediff

--- linediff.py
+++ linediff.py
@@ -2,11 +2,14 @@
 
 """Display differences between two files, line by line."""
 
+import re
 from signal import signal, SIGPIPE, SIG_DFL
 import sys
 from typing import List, Tuple
 
 import diff_match_patch  # type: ignore
+
+SHOW_WORDS = 3
 
 
 def strikethrough(s: str):
@@ -22,36 +25,52 @@ def strikethrough(s: str):
     return "".join(cs)
 
 
-def print_diff(diff: List[Tuple[int, str]], show_del: bool = True):
+def print_diff(diff: List[Tuple[int, str]], show_del: bool = False):
     """Pretty-print the result of the line diff."""
-    LONG = 18  # how long is a "long" amount of common text?
-    # abbreviate a common prefix
-    if diff and diff[0][0] == 0 and len(diff[0][1]) > LONG:
-        words = diff[0][1].split(" ")
-        diff[0] = (0, "…" + " ".join(words[-4:]))
 
-    if diff and diff[-1][0] == 0 and len(diff[-1][1]) > LONG:
-        words = diff[-1][1].split(" ")
-        suffix = " ".join(words[:4])
-        while (
-            suffix.endswith(".")
-            or suffix.endswith(")")
-            or suffix.endswith(",")
-        ):
-            suffix = suffix[:-1]
-        diff[-1] = (0, suffix + "…")
-
-    for (n, s) in diff:
+    num_diffs = len(diff)
+    for i in range(num_diffs):
+        n, s = diff[i]
         if n == 0:
-            if len(s) > 2 * LONG and "…" not in s:
-                s = s[: (LONG - 1)] + "…" + s[-(LONG - 1) :]
+            words = re.split("\s+", s)
+            num_words = len(words)
+
+            # Abbreviate text with lots of words
+            if i == 0 and num_words > SHOW_WORDS:
+                # omit leading words in the sentence
+                s = "…" + " ".join(words[-SHOW_WORDS:])
+            elif i == num_diffs - 1 and num_words > SHOW_WORDS:
+                # omit trailing words in the sentence
+                s = " ".join(words[:SHOW_WORDS])
+                while s.endswith(".") or s.endswith(")") or s.endswith(","):
+                    # delete trailing punctuation too
+                    s = s[:-1]
+                s = s + "…"
+            elif num_words > SHOW_WORDS * 2:
+                # omit middle words in the middle of a sentence
+                #
+                # when s ends with a space (which is common), the last word
+                # in the list words will be an empty string.
+                # In that case, we emit one extra (hopefully visible) word
+                # after the ellipsis, so there are SHOW_WORDS visible words.
+                s = (
+                    " ".join(words[:SHOW_WORDS])
+                    + "…"
+                    + " ".join(
+                        words[-SHOW_WORDS - (1 if s[-1] == " " else 0) :]
+                    )
+                )
+
             print(s, end="")
-        elif n == -1 and show_del:
-            print(
-                "\033[31m\033[1m" + strikethrough(s) + "\033[0m",
-                end="",
-            )
+        elif n == -1:
+            if show_del or i + 1 < num_diffs and diff[i + 1][0] == 0:
+                print(
+                    "\033[31m\033[1m" + strikethrough(s) + "\033[0m",
+                    end="",
+                )
         elif n == 1:
+            if s.strip() == "":
+                s = s.replace(" ", "⎵")
             print(
                 "\033[32m\033[1m" + s + "\033[0m",
                 end="",
