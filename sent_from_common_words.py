85dab6a408e95b38218c3ad3e60b3f6e96d5877d
hhammer1001 <hhammer@hmc.edu>
2022-06-13 14:05:00 -0700

    fig fixes

--- sent_from_common_words.py
+++ sent_from_common_words.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 import argparse
-from collections import Counter
+from collections import Counter, defaultdict
+from email.policy import default
 import sys
 
 
@@ -37,24 +38,22 @@ common_words = f.readlines()
 f.close()
 common_words = common_words[1:]
 common_words = [x.split("\t")[1][:-1] for x in common_words]
-common_words = common_words[:200]
+common_words = common_words[:100]
 punctuation.update(common_words)
 # print(punctuation)
 
 
 def main(args):
-    bigcount = 0
-    count = 0
+    counts = defaultdict(lambda: 0)
+    counts["Total"] = 38041358
     for fd in args.files:
         for line in fd:
             proofID, text = line.strip().split("\t")
             words = set([w.lower() if w not in aliases else w
                     for w in text.split(" ")
                     if (w.lower() not in punctuation and w not in punctuation)])
-            if len(words) < 3:
-                count += 1
-            bigcount += 1
-    print(f"{count} out of {bigcount}")
+            counts[len(words)] += 1
+    print(counts)
     # for proofID, words in word_dict.items():
     #     print(f"{proofID}\t{' '.join(list(words))}")
 
