bc73ae88cebb626c0b2dc698f1df22cd844a4889
hhammer1001 <hhammer@hmc.edu>
2022-06-13 16:50:24 -0700

    sent common words uses dicts now

--- sent_from_common_words.py
+++ sent_from_common_words.py
@@ -38,21 +38,31 @@ common_words = f.readlines()
 f.close()
 common_words = common_words[1:]
 common_words = [x.split("\t")[1][:-1] for x in common_words]
-common_words = common_words[:100]
+common_words = common_words[:50]
 punctuation.update(common_words)
 # print(punctuation)
 
 
 def main(args):
-    counts = defaultdict(lambda: 0)
-    counts["Total"] = 38041358
+    counts = {}
+    for x in range(56):
+        counts[x] = 0
     for fd in args.files:
         for line in fd:
             proofID, text = line.strip().split("\t")
             words = set([w.lower() if w not in aliases else w
                     for w in text.split(" ")
                     if (w.lower() not in punctuation and w not in punctuation)])
-            counts[len(words)] += 1
+            if len(words) in counts:
+                counts[len(words)] += 1
+            else:
+                counts[len(words)] = 1
+        counts["Total"] = 38041358
+    for x in range(150):
+        if x in counts and counts[x] > 5:
+            print(f"{x} {counts[x]}")
+
+
     print(counts)
     # for proofID, words in word_dict.items():
     #     print(f"{proofID}\t{' '.join(list(words))}")
