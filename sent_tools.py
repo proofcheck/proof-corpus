26820f7d83552fd3bd3d327b7990d45e557cf45f
Nanako Noda <nnoda@hmc.edu>
2022-06-14 16:48:26 -0700

    Added function for reading multiple files

--- sent_tools.py
+++ sent_tools.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python
 
+from multiprocessing import Pool
+import nicer
+
 def split_sentence_id(line):
     line = line.strip()
     if "\t" in line:
@@ -24,11 +27,65 @@ def split_sentences_ids(lines):
 
     return ids, sents
 
-def tokenizer(sent):
+def tokenize(sent):
     tokenized = sent.split(" ")
     return tokenized
 
 def tokenize_sentences(sents):
-    tokenized_sents = [tokenizer(sent) for sent in sents]
+    tokenized_sents = [tokenize(sent) for sent in sents]
     return tokenized_sents
 
+def split_sentence_id_tokenized(line):
+    sent_id, sent = split_sentence_id(line)
+    tokenized = tokenize(sent)
+    return sent_id, tokenized
+
+def read_one(fn):
+    # Input: file of sentences/proofs
+    # Returns list of ids and sentences
+    f = open(fn, "r")
+    lines = f.readlines()
+    ids, sents = split_sentences_ids(lines)
+    f.close()
+    return ids, sents
+
+def read_one_tokenized(fn):
+    # Input: file of sentences/proofs
+    # Returns list of ids and tokenized sentences
+    ids, sents = read_one(fn)
+    tokenized = tokenize_sentences(sents)
+    return ids, tokenized
+
+def read_files(files, cores):
+    nicer.make_nice()
+    ids = []
+    sents = []
+    for fd in files:
+        print(fd)
+        with Pool(processes=cores) as p:
+            for id_sent in p.imap(
+                    split_sentence_id,
+                    fd.readlines(),
+                    250
+                ):
+                ids += id_sent[0]
+                sents += id_sent[1]
+        fd.close()
+    return ids, sents
+
+def read_files_tokenized(files, cores):
+    nicer.make_nice()
+    ids = []
+    sents = []
+    for fd in files:
+        print(fd)
+        with Pool(processes=cores) as p:
+            for id_sent in p.imap(
+                    split_sentence_id_tokenized,
+                    fd.readlines(),
+                    250
+                ):
+                ids += id_sent[0]
+                sents += id_sent[1]
+        fd.close()
+    return ids, sents
