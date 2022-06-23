54490b53c1e544c98e57301e3614e16e53f348b0
hhammer1001 <hhammer@hmc.edu>
2022-06-22 17:01:21 -0700

    cleanup fixes

--- cleanup.py
+++ cleanup.py
@@ -540,7 +540,7 @@ def cleanup(
 
         proof = re.sub(r"\(\s*([-0-9]+|[a-zA-Z])(\s*,\s*([0-9.,'-]+|[a-zA-Z])\s*)+\)", "MATH", proof)
 
-        proof = re.sub(r"(?i:(figure|fig))\s*[.]?\s*[0-9]*(\s*REF)?", "REF", proof)
+        proof = re.sub(r"\b(?i:(figure|fig))\s*[.]?\s*[0-9]*(\s*REF)?", "REF", proof)
 
     # eliminate extra spaces
     proof = re.sub("[ ]+", " ", proof)
