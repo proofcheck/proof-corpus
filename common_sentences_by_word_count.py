4fe9c8366d637c51d56324b5a6593992c39ef248
nanakonoda <nnoda@hmc.edu>
2023-03-17 19:16:22 -0700

    Combined read_file function

--- common_sentences_by_word_count.py
+++ common_sentences_by_word_count.py
@@ -10,9 +10,10 @@ import sys
 from collections import Counter
 
 from sent_tools import *
-# SORTED_PATH = "/research/proofcheck/noda/proof-corpus/sorted_small.txt"
-WC_PATH = "/research/proofcheck/noda/proof-corpus/sorted_small_wc.txt"
+SMALL_SORTED_PATH = "/research/proofcheck/noda/proof-corpus/sorted_small.txt"
+SMALL_WC_PATH = "/research/proofcheck/noda/proof-corpus/sorted_small_wc.txt"
 SORTED_PATH = "/research/proofcheck/2023-01-04/sorted.txt"
+WC_PATH = "/research/proofcheck/noda/proof-corpus/sorted_wc.txt"
 
 def check_alias(word, alias_list):
     # Check if word contains an alias
@@ -77,79 +78,90 @@ def make_tuple_from_word_count(line):
     num_count, sent, word_count = line.split("\t")
     return int(num_count), sent, int(word_count)
 
-def read_sorted(filename=SORTED_PATH, cores=4):
+def read_file(filename=SORTED_PATH, cores=4):
+    if filename is SORTED_PATH:
+        sorted = True
+        make_tuple = make_tuple_from_sorted
+
+    else:
+        sorted = False
+        make_tuple = make_tuple_from_word_count
+        
     with Pool(processes=cores) as p:
+        with open(filename, "r") as fd:
+            sent_counter = Counter()
+            for sent_tuple in p.imap(
+                    make_tuple,
+                    fd.readlines(),
+                    50
+                    ) :
+                        num_count, sent, word_count = sent_tuple
+                        sent_counter.update({sent : num_count})
+
+                        if sorted:
+                            content += str(num_count) + "\t" + sent + "\t" + str(word_count) + "\n"
+
+    if sorted:
         with open(WC_PATH, "w") as o:
-            with open(filename, "r") as fd:
-                # sent_tuples = []
-                sent_counter = Counter()
-                for sent_tuple in p.imap(
-                    make_tuple_from_sorted,
-                            fd.readlines(),
-                        50
-                        ) :
-                            num_count, sent, word_count = sent_tuple
-                            # if sent in sent_counter.keys():
-                            sent_counter.update({sent : num_count})
-                            # else:
-                            # sent_counter.update({ sent : num_count })
-                            
+            o.write(content)
+
     return sent_counter
 
-def read_word_count_file(fd, cores):
-    with Pool(processes=cores) as p:
-        sent_tuples = []
-        sent_tuples += p.imap(
-            make_tuple_from_word_count,
-                    fd.readlines(),
-                50
-                )
-    return sent_tuples
+# def read_word_count_file(fd, cores):
+#     with Pool(processes=cores) as p:
+#         with open(filename, "r") as fd:
+#             sent_counter = Counter()
+#             for sent_tuple in p.imap(
+#                 make_tuple_from_word_count,
+#                         fd.readlines(),
+#                     50
+#                     ) :
+#                         num_count, sent, word_count = sent_tuple
+#                         sent_counter.update({sent : num_count})
 
-def write_results(sent_counter, output):
-    # sent_list = [ [str(x) for x in sent] for sent in sent_list]
-    for sent, num_count in sent_counter.items():
-        sent_formatted = num_count + "\t" + sent + "\t" + count_words(sent) + "\n"
+#     return sent_tuples
+
+def write_results(common_sents, output):
+    for sent, num_count in common_sents:
+        sent_formatted = str(num_count) + "\t" + sent + "\t" + str(count_words(sent)) + "\n"
         output.write(sent_formatted)
 
-def filter_by_word_count(sent_tuple, word_count):
-    if sent_tuple[2] == word_count:
-        return sent_tuple
+def sent_is_word_count(sent, word_count):
+    if not word_count:
+        return sent, True
+    elif count_words(sent) == word_count:
+        return sent, True
+    else:
+        return sent, False
 
 def main(args):
-    if args.file:
-        sent_tuples = read_word_count_file(args.file, args.cores)
-    else:
-        sent_tuples = read_sorted(cores=args.cores)
+    if not args.file:
+        sent_counter = read_file(cores=args.cores)
     
+    else:
+        sent_counter = read_file(args.file, args.cores)
+        
+    sents = sent_counter.keys()
     with Pool(processes=args.cores) as p:
-        common_n = p.starmap(
-                        filter_by_word_count,
+        for sent, is_word_count in p.starmap(
+                        sent_is_word_count,
                             zip(
-                                sent_tuples,
+                                sents,
                                 repeat(args.word_count),
                             ),
                             50,
-                        )
-    common_n = list(filter(lambda x: x is not None, common_n))
-    common_n = common_n[:args.n_common]
+        ):
+            if is_word_count == False:
+                del sent_counter[sent]
 
-    # for sent in sent_tuples:
-    #     if sent[2] == args.word_count:
-    #         print(sent)
-    #         common_n += [sent]
-    #     else:
-    #         continue
-    #     if len(common_n) == args.n_common:
-    #         break
-    
-    write_results(common_n, args.output)
-    
+    common = sent_counter.most_common(args.n_common)
+    write_results(common, args.output)
+
 if __name__ == "__main__":
     nicer.make_nice()
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--file", "-f", type=argparse.FileType("r"),
+    parser.add_argument("--file", "-f",
                         help="tsv file to read sentences with word counts from")
 
     parser.add_argument("--cores", "-p", type=int, default=4,
@@ -158,10 +170,10 @@ if __name__ == "__main__":
     parser.add_argument("--output", "-o", type=argparse.FileType("w"), default=sys.stdout,
                         help="output file")
     
-    parser.add_argument("--n_common", "-n", type=int, default=10,
+    parser.add_argument("--n_common", "-n", type=int, default=None,
                         help="number of common sentences to find")
 
-    parser.add_argument("--word_count", "-wc", type=int, default=5,
+    parser.add_argument("--word_count", "-w", type=int, default=None,
                         help="number of words in sentence")
     
     args = parser.parse_args()
